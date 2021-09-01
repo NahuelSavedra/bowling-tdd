447117b7a037b7ff0eed563bafc453708a196cf5
Nahuel Savedra <nsavedra97@gmail.com>
2021-09-01 17:27:39 -0300

    added another test method

--- src/test/java/com/example/BowlingGame/BowlingGame.java
+++ src/test/java/com/example/BowlingGame/BowlingGame.java
@@ -1,4 +1,12 @@
 package com.example.BowlingGame;
 
 public class BowlingGame {
+    private int score = 0;
+    public void roll(int pins) {
+        score += pins;
+    }
+
+    public int score() {
+        return score;
+    }
 }
